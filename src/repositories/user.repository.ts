d66fa0a6a894d9192c25669582dc913e022ceeff
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-11-03 09:36:48 +0700

    implement init user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,47 @@
+import { knex } from "knex";
+import Logging from "../common/Logging";
+
+import { knexConfig } from "../config/knexfile";
+import { User } from "../models/user.model";
+
+const TableName = "user";
+export class UserRepository {
+  private db = knex(knexConfig);
+
+  constructor() {}
+
+  // Get all
+  async getAll(): Promise<User[]> {
+    const categories = await this.db<User>(TableName).select();
+    return categories;
+  }
+
+  // Get by Name
+  async getByName(name: string): Promise<User | null> {
+    const category = await this.db<User>(TableName)
+      .where("username", name)
+      .orWhere("email", "like", "%${name}%")
+      .first();
+    return category ?? null;
+  }
+
+  // Get by id
+  async getById(id: number): Promise<User | null> {
+    const category = await this.db<User>(TableName).where("id", id).first();
+    return category ?? null;
+  }
+
+  // Create
+  async create(model: User): Promise<User> {
+    const [id] = await this.db<User>(TableName).insert(model).returning("id");
+    model.id = Number(id);
+    Logging.info(model);
+    return model;
+  }
+
+  // Update
+  async update(model: User): Promise<User> {
+    await this.db<User>(TableName).where("id", model.id).update(model);
+    return model;
+  }
+}
