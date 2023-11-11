9cc1b73ef184a5b8f5890d84c67995febcc41f75
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-11-11 18:35:40 +0700

    update order repository

--- src/repositories/order.item.repository.ts
+++ src/repositories/order.item.repository.ts
@@ -1,20 +1,24 @@
 import { knex } from "knex";
 import config from "../config/config";
+import { OrderItem } from "../models/orderItem.model";
 import { OrderItemDTO } from "../Dtos/order/order.item.dto";
 
 class OrderItemRepository {
   private db = knex(config.knex);
-  private table = "orders";
+  private table = "order_items";
   constructor() {}
 
-  async getAll(): Promise<OrderItemDTO[]> {
+  async getAll(): Promise<OrderItem[]> {
     return await this.db(this.table).select("*");
   }
 
-  async getById(id: number): Promise<OrderItemDTO | null> {
-    return await this.db(this.table).where({ id: id }).first();
+  async getById(orderId: number): Promise<OrderItemDTO[] | null> {
+    const orderItems = await this.db<OrderItemDTO>(this.table).where({
+      orderId: orderId,
+    });
+    return orderItems || null;
   }
-  async search(searchCriteria: Partial<OrderItemDTO>): Promise<OrderItemDTO[]> {
+  async search(searchCriteria: Partial<OrderItem>): Promise<OrderItem[]> {
     let query = this.db(this.table);
 
     for (const [key, value] of Object.entries(searchCriteria)) {
@@ -24,16 +28,19 @@ class OrderItemRepository {
     return await query.select("*");
   }
 
-  async create(item: OrderItemDTO): Promise<OrderItemDTO> {
+  async insert(item: OrderItemDTO): Promise<OrderItemDTO | null> {
     const [newId] = await this.db(this.table).insert(item);
-    return await this.db(this.table).where({ id: newId }).first();
+    const result = await this.db<OrderItemDTO>(this.table)
+      .where({ id: newId })
+      .first();
+    return result || null;
   }
 
-  async read(id: number): Promise<OrderItemDTO | null> {
+  async read(id: number): Promise<OrderItem | null> {
     return await this.db(this.table).where({ id }).first();
   }
 
-  async update(id: number, item: OrderItemDTO): Promise<OrderItemDTO> {
+  async update(id: number, item: OrderItem): Promise<OrderItem> {
     await this.db(this.table).where({ id: id }).update(item);
     return await this.db(this.table).where({ id: id }).first();
   }
@@ -42,3 +49,4 @@ class OrderItemRepository {
     await this.db(this.table).where({ id }).delete();
   }
 }
+export default new OrderItemRepository();
--- src/repositories/order.repository.ts
+++ src/repositories/order.repository.ts
@@ -1,5 +1,6 @@
 import { knex } from "knex";
 import config from "../config/config";
+import { Order } from "../models/order.model";
 import { OrderDTO } from "../Dtos/order/order.dto";
 
 class OrderRepository {
@@ -7,14 +8,17 @@ class OrderRepository {
   private table = "orders";
   constructor() {}
 
-  async getAll(): Promise<OrderDTO[]> {
-    return await this.db(this.table).select("*");
+  async getAll(userId: number): Promise<OrderDTO[]> {
+    return await this.db(this.table)
+      .where("userId", userId)
+      .orderBy("createdDate", "desc")
+      .select("*");
   }
 
   async getById(id: number): Promise<OrderDTO | null> {
     return await this.db(this.table).where({ id: id }).first();
   }
-  async search(searchCriteria: Partial<OrderDTO>): Promise<OrderDTO[]> {
+  async search(searchCriteria: Partial<Order>): Promise<Order[]> {
     let query = this.db(this.table);
 
     for (const [key, value] of Object.entries(searchCriteria)) {
@@ -24,16 +28,17 @@ class OrderRepository {
     return await query.select("*");
   }
 
-  async create(item: OrderDTO): Promise<OrderDTO> {
+  async create(item: OrderDTO): Promise<Order | null> {
     const [newId] = await this.db(this.table).insert(item);
-    return await this.db(this.table).where({ id: newId }).first();
+    const model = await this.db(this.table).where({ id: newId }).first();
+    return model || null;
   }
 
-  async read(id: number): Promise<OrderDTO | null> {
+  async read(id: number): Promise<Order | null> {
     return await this.db(this.table).where({ id }).first();
   }
 
-  async update(id: number, item: OrderDTO): Promise<OrderDTO> {
+  async update(id: number, item: Order): Promise<Order> {
     await this.db(this.table).where({ id: id }).update(item);
     return await this.db(this.table).where({ id: id }).first();
   }
@@ -42,3 +47,4 @@ class OrderRepository {
     await this.db(this.table).where({ id }).delete();
   }
 }
+export default new OrderRepository();
